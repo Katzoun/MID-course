ee7d605b00772d6c2abc33b8b21cbac332ed043c
Tomas <tomas.janousek02@gmail.com>
2025-09-25 09:30:57 +0200

    Init

---
+++ ex/ukol1.m
@@ -0,0 +1,32 @@
+clc, clear, close all
+
+Ts = 0.01
+t = 0:Ts:10;
+u = ones(size(t));
+
+
+y = cviceni02_1(u,t);
+
+figure(1);
+plot(t,y);
+hold on
+
+K = mean(y(end-2:end));
+d = (y(2)-y(1))/Ts;
+T = K/d;
+
+disp('Identifikovany prenos')
+p = tf('p'); 
+F = K / (T*p + 1);
+
+% ted zkusime odezvu naseho identifikovaneho sys
+h = K*(1-exp(-t/T));
+plot(t,h)
+
+% jeste jina varianta
+yv =lsim(F,u,t);
+plot(t,yv)
+
+legend('vystup systemu', 'vystup ident sys', 'vystup z lsim')
+xlabel('t')
+ylabel('y')
